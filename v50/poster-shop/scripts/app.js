79d01b3c9d524003831c4a693ffb1bc0e4f59456
Nennae <sezenerdal@outlook.com>
2024-12-18 12:07:50 +0100

    responsive grid layout implemented

--- v50/poster-shop/scripts/app.js
+++ v50/poster-shop/scripts/app.js
@@ -14,12 +14,14 @@ function displayProducts(products) {
         
     productDiv.innerHTML = `
       <img class="char-img" src="${product.image.desktop}" alt="${product.name}" />
+      <div class="product-card">
       <h2>${product.name}</h2>
       <p>${product.description}</p>
       <p>Price: ${product.price.toFixed(2)} SEK</p>
-      <button class="add-to-cart-btn" data-name="${product.name}" data-price="${
+      <button class="small-btn" data-name="${product.name}" data-price="${
       product.price
     }">Add to Cart</button>
+    </div>
       <div class="quantity-controls" style="display: none;">
         <button class="decrease-btn">-</button>
         <span class="quantity" data-name="${product.name}">1</span>
