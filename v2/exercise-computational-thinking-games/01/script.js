9ed0f832714c7cd1bbd898ecb71a5f68b69e666d
Nennae <sezenerdal@outlook.com>
2025-01-12 23:12:52 +0100

    array exercises commit

--- v2/exercise-computational-thinking-games/01/script.js
+++ v2/exercise-computational-thinking-games/01/script.js
@@ -1,9 +1,3 @@
-// Throw the dice
-// Generate random numbers between 1 - 6 for each throw
-// If first number is 1, add it to the latter-list and so on
-// If not, keep throwing the dice until the latter is reached in order 1,2,3,4,5,6
-// Meanwhile, count each throw during the whole process
-// End the game when latter is achieved with the total number of throws
 
 const throwDice = () => {
       const latterNums = [1, 2, 3, 4, 5, 6];
---
+++ v2/exercise-computational-thinking-games/02/script.js
@@ -0,0 +1,16 @@
+
+function throwDices() {
+      let knockOutNums = [6, 7, 8, 9];
+      let targetEndScore = 12;
+      let playerScore = 0;
+      let playerThrowResults = [];
+
+      while (playerThrowResults.length < 3) {
+            let generateDiceNum = Math.floor(Math.random() * 6 + 1);
+            playerThrowResults.push(generateDiceNum);
+
+            let max = playerThrowResults.reduce((num))
+      }
+}
+
+throwDices();
